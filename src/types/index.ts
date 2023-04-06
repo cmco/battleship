2b69657bdc969c5f14f0654945545b6ee249262e
cedmco <cedrickc@openpay.com.au>
2023-04-06 14:20:49 +0800

    Add type declarations file for TS

---
+++ src/types/index.ts
@@ -0,0 +1,80 @@
+import { Dispatch, SetStateAction } from 'react';
+
+type ShipNames =
+  | 'carrier'
+  | 'cruiser'
+  | 'battleship'
+  | 'destroyer'
+  | 'submarine';
+
+export interface ShipInfo {
+  size: number;
+  count: number;
+}
+
+interface ShipTypes {
+  carrier: ShipInfo;
+  cruiser: ShipInfo;
+  battleship: ShipInfo;
+  destroyer: ShipInfo;
+  submarine: ShipInfo;
+}
+
+type Coordinates = [number, number];
+
+export interface Layout {
+  ship: ShipNames;
+  positions: Coordinates[];
+}
+
+export interface apiData {
+  shipTypes: ShipTypes;
+  layout: Layout[];
+}
+
+export interface ShipStateObject {
+  ship: string;
+  count: number;
+  size: number;
+}
+
+export type ShipsState = Record<string, ShipStateObject>;
+
+export interface CellProps {
+  hasShip: boolean;
+  id?: string;
+  setShipsState?: Dispatch<SetStateAction<ShipsState>>;
+  setTotalHits?: Dispatch<SetStateAction<number>>;
+}
+
+export interface StyledCellProps {
+  isMarked: boolean;
+}
+
+export interface ShipTracker {
+  size: number;
+  hits: number;
+}
+
+export interface ShipsTrackerObject {
+  carrier: ShipTracker;
+  battleship: ShipTracker;
+  cruiser: ShipTracker;
+  submarine: ShipTracker;
+  destroyer: ShipTracker;
+}
+
+export interface StyledPlayerPoints {
+  playerOne?: boolean;
+}
+
+export interface PlayerPointsProps {
+  totalHits: number;
+  playerOne?: boolean;
+}
+
+export interface TrackerProps {
+  ship: string;
+  size: number;
+  count: number;
+}
